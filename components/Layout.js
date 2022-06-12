2059bb46f71174ad88c6b37eb6b8fc6a1142b781
Mubashira M K <mubashiramk07@gmail.com>
2022-06-12 18:50:29 +0530

    Dark Mode

--- components/Layout.js
+++ components/Layout.js
@@ -1,17 +1,58 @@
-import React from "react";
+import React, { useContext } from "react";
 import Head from "next/head";
 import useStyle from "../utils/styles";
 import NextLink from "next/link";
+// import { createTheme } from "@mui/material/styles";
+// import CssBaseline from "@mui/material/CssBaseline";
 import {
   AppBar,
   Container,
   Toolbar,
   Typography,
   Link,
+  createTheme,
+  ThemeProvider,
+  CssBaseline,
+  Switch,
 } from "@material-ui/core";
+import { Store } from "../utils/Store";
+import Cookies from "js-cookie";
 
 export default function Layout({ title, description, children }) {
+  const { state, dispatch } = useContext(Store);
+  const { darkMode } = state;
   const classes = useStyle();
+  const theme = createTheme({
+    typography: {
+      fontSize: 15,
+      h1: {
+        fontSize: "1.6rem",
+        fontWeight: 400,
+        margin: "1rem 0",
+      },
+      h2: {
+        fontSize: "1.4rem",
+        fontWeight: 400,
+        margin: "1rem 0",
+      },
+    },
+    palette: {
+      type: darkMode ? "dark" : "light",
+      primary: {
+        main: "#f0c000",
+      },
+      secondary: {
+        main: "#208080",
+      },
+    },
+  });
+
+  const darkModeChangeHandler = () => {
+    dispatch({ type: darkMode ? "DARK_MODE_OFF" : "DARK_MODE_ON" });
+    const newDarkMode = !darkMode;
+    Cookies.set("darkMode", newDarkMode ? "ON" : "OFF");
+  };
+
   return (
     <div>
       <Head>
@@ -22,29 +63,36 @@ export default function Layout({ title, description, children }) {
         </title>
         {description && <meta name="description" content={description}></meta>}
       </Head>
-      <AppBar position="static" className={classes.navbar}>
-        <Toolbar>
-          <NextLink href="/" passHref>
-            <Link>
-              <Typography className={classes.typography}>Flamingo</Typography>
-            </Link>
-          </NextLink>
-          <div className={classes.grow}></div>
-          <div>
-            <NextLink href="/cart" passHref>
-              <Link>Cart </Link>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="static" className={classes.navbar}>
+          <Toolbar>
+            <NextLink href="/" passHref>
+              <Link>
+                <Typography className={classes.typography}>Flemingo</Typography>
+              </Link>
             </NextLink>
+            <div className={classes.grow}></div>
+            <div>
+              <Switch
+                checked={darkMode}
+                onChange={darkModeChangeHandler}
+              ></Switch>
+              <NextLink href="/cart" passHref>
+                <Link>Cart </Link>
+              </NextLink>
 
-            <NextLink href="/login" passHref>
-              <Link> Login</Link>
-            </NextLink>
-          </div>
-        </Toolbar>
-      </AppBar>
-      <Container className={classes.main}>{children}</Container>
-      <footer className={classes.footer}>
-        <Typography>All rights reserved. Flemingo e-commerce</Typography>
-      </footer>
+              <NextLink href="/login" passHref>
+                <Link> Login</Link>
+              </NextLink>
+            </div>
+          </Toolbar>
+        </AppBar>
+        <Container className={classes.main}>{children}</Container>
+        <footer className={classes.footer}>
+          <Typography>All rights reserved. Flemingo e-commerce</Typography>
+        </footer>
+      </ThemeProvider>
     </div>
   );
 }
